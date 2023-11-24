fa132d935c029571319d03276b3c98715d29c992
amr <amr.elsaeed995@gmail.com>
2023-11-24 13:48:46 +0200

    auth

--- api/views.py
+++ api/views.py
@@ -1,16 +1,44 @@
 from rest_framework import request, status, viewsets
 from .models import Meal, Rating
-from .serializers import MealSerializer, RatingSerializer
+from .serializers import MealSerializer, RatingSerializer, UserSerializer
 
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from django.contrib.auth.models import User
 
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny, ]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        token, created = Token.objects.get_or_create(user=serializer.instance)
+        return Response({
+                'token': token.key, 
+                }, 
+            status=status.HTTP_201_CREATED)
+    
+    def list(self, request, *args, **kwargs):
+        response = {'message': 'You cant create rating like that'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+    
+
 
 class MealViewSet(viewsets.ModelViewSet):
     queryset = Meal.objects.all()
     serializer_class = MealSerializer
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticated,)
 
     @action(detail=True, methods=['post'])
     def rate_meal(self, request, pk=None):
@@ -20,8 +48,10 @@ class MealViewSet(viewsets.ModelViewSet):
             '''
             meal = Meal.objects.get(id=pk)
             stars = request.data['stars']
-            username = request.data['username']
-            user = User.objects.get(username=username)
+            user = request.user 
+            # print(user)
+            # username = request.data['username']
+            # user = User.objects.get(username=username)
 
             try:
                 # update
@@ -58,4 +88,18 @@ class MealViewSet(viewsets.ModelViewSet):
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
- 
+
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticated)
+
+    def update(self, request, *args, **kwargs):
+        response = {
+            'message': 'invalid way to create or update',
+        }
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+    
+    def create(self, request, *args, **kwargs):
+        response = {
+            'message': 'invalid way to create or update',
+        }
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
