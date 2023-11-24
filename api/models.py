c8c3ba5ee04cf867387a16df10eb7efb4469af86
amr <amr.elsaeed995@gmail.com>
2023-11-24 05:36:15 +0200

    commit

--- api/models.py
+++ api/models.py
@@ -2,11 +2,27 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-# uuid
 
 class Meal(models.Model):
-    title = models.CharField(max_length=50)
-    description = models.TextField(max_length=300)
+    title = models.CharField(max_length=32)
+    description = models.TextField(max_length=360)
+
+    def no_of_ratings(self):
+        ratings = Rating.objects.filter(meal=self)
+        return len(ratings)
+    
+    def avg_rating(self):
+        # sum of ratings stars  / len of rating hopw many ratings 
+        sum = 0
+        ratings = Rating.objects.filter(meal=self) # no of ratings happened to the meal 
+
+        for x in ratings:
+            sum += x.stars
+
+        if len(ratings) > 0:
+            return sum / len(ratings)
+        else:
+            return 0
 
     def __str__(self):
         return self.title
@@ -17,12 +33,10 @@ class Rating(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     stars = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
 
-    #def __str__(self):
-     #   return self.meal 
+    # def __str__(self):
+    #     return self.meal
+
 
     class Meta:
-        unique_together = (('user', 'meal'),)  #user my3rfsh y3ml rate aktr mn meal
+        unique_together = (('user', 'meal'),)
         index_together = (('user', 'meal'),)
-
-
-    
